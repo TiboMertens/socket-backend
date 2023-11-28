1d779bf6a834b97c4e2e58ac672f2f7ec87aa374
TiboMertens <tibomertens25@gmail.com>
2023-11-28 16:18:16 +0100

    feat: add primus server connection

---
+++ primus/live.js
@@ -0,0 +1,20 @@
+module.exports.go = (server) => {
+  const Primus = require("primus");
+  const primus = new Primus(server, {});
+
+  //check if primus is working, then log
+  primus.on("connection", (spark) => {
+    console.log("connected");
+
+    //check if data received from client, then log
+    spark.on("data", (data) => {
+      console.log(data);
+      if (data.action === "newMessage") {
+        primus.write({
+          action: "newMessage",
+          message: data.team,
+        });
+      }
+    });
+  });
+};
